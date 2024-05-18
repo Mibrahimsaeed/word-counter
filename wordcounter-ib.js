b940c188ac9b7fb52b9b5bb42b2bdeb4ee7adbff
Muhammad Ibrahim <143446305+Mibrahimsaeed@users.noreply.github.com>
2024-05-18 15:08:00 +0500

    Add files via upload

---
+++ wordcounter-ib.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+function count(paragraph, usrinp) {
+    const WithoutSpaces = paragraph.replace(/\s/g, '');
+    const characterCount = WithoutSpaces.length;
+    const wordCount = WithoutSpaces.split(/\s+/).filter(word => word !== '').length;
+    return { characters: characterCount, words: wordCount };
+}
+// const paragraph = "This is a sample paragraph. It contains several words and spaces.";
+// const counts = count(paragraph);
+// console.log("Character count:", counts.characters);
+// console.log("Word count:", counts.words);
+import inquirer from 'inquirer';
+const wordcounter = await inquirer.prompt([{
+        "type": "input",
+        "name": "usrinp",
+        "message": "Enter a paragraph"
+    }]);
+const counts = count(wordcounter.usrinp);
+console.log("Your counts are ", counts);
